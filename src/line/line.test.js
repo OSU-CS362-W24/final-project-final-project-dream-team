2b1f937a0e8651bc99bcd7d68eb234ef71460a37
Madison Dowell <dowellma@oregonstate.edu>
2024-03-11 18:43:40 -0700

    Set up jsdom testing enviornment

--- src/line/line.test.js
+++ src/line/line.test.js
@@ -1,3 +1,9 @@
 /**
 * @jest-environment jsdom
 */
+
+const fs = require("fs")
+
+require("@testing-library/jest-dom")
+const domTesting = require("@testing-library/dom")
+const userEvent =require("@testing-library/user-event").default

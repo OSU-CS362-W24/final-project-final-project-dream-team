bef2a99db7b8cd04d5b48668af87c9e5ea848628
Madison Dowell <dowellma@oregonstate.edu>
2024-03-12 17:06:57 -0700

    Create test for clicking generate chart button while only providing x label

--- src/line/line.test.js
+++ src/line/line.test.js
@@ -141,4 +141,25 @@ test("When a user clicks the generate chart button after only providing a Y labe
 
 })
 
+test("When a user clicks the generate chart button after only providing a X label, an error message is displayed", async function() {
+    // Arrange:
+    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
+
+    const mockAlert = jest.spyOn(window, 'alert').mockImplementation(() => {})
+
+    // Acquire the input field
+    const generateChart = domTesting.getByText(document, 'Generate chart')
+    const XLabel = domTesting.getByLabelText(document, 'X label')
+
+    // Act:
+    const user = userEvent.setup()
+    await user.type(XLabel, 'Time')
+    await user.click(generateChart)
+
+    // Assert:
+    expect(mockAlert).toHaveBeenCalledTimes(1)
+    mockAlert.mockRestore()
+
+})
+
 //const xLabel = domTesting.getByLabelText(document, 'X label')
